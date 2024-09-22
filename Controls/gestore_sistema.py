37ba68b031a000ae96750fb303c5dbc5e29daedd
sliberatoscioli <sara.liberatoscioli2003@gmail.com>
2024-09-23 00:41:16 +0200

    nuovo backup

--- Controls/gestore_sistema.py
+++ Controls/gestore_sistema.py
@@ -5,30 +5,30 @@ import shutil
 class GestoreBackup:
     def __init__(self):
         # Determina il percorso del desktop dell'utente
-        self.desktop_path = os.path.join(os.path.expanduser("~"), "Desktop") # Determina il percorso del desktop dell'utente
-        self.backup_folder = os.path.join(self.desktop_path, 'Backup')     # Crea la cartella 'Backup' sul desktop
-        self.data_folder = os.path.join(os.getcwd(), "Dati")
+        self.percorso_desktop = os.path.join(os.path.expanduser("~"), "Desktop") # Determina il percorso del desktop dell'utente
+        self.cartella_backup = os.path.join(self.percorso_desktop, 'Backup')     # Crea la cartella 'Backup' sul desktop
+        self.cartella_dati = os.path.join(os.getcwd(), "Dati")
         self.files_to_backup = ["Clienti.pkl", "Dipendenti.pkl", "Prodotti.pkl", "Acquisti.pkl"]
         self.msg_box = QMessageBox()
 
     def backup(self):
         # Crea la cartella 'Backup' se non esiste e copia i file
-        if not os.path.exists(self.backup_folder):
-            os.makedirs(self.backup_folder)
-            self.msg_box.setText(f"Cartella di backup creata in: {self.backup_folder}")
+        if not os.path.exists(self.cartella_backup):
+            os.makedirs(self.cartella_backup)
+            self.msg_box.setText(f"Cartella di backup creata in: {self.cartella_backup}")
             self.msg_box.setIcon(QMessageBox.Warning)
             self.msg_box.exec_()
         else:
-            self.msg_box.setText(f"La cartella di backup esiste già in: {self.backup_folder}")
+            self.msg_box.setText(f"La cartella di backup esiste già in: {self.cartella_backup}")
             self.msg_box.setIcon(QMessageBox.Warning)
             self.msg_box.exec_()
 
         # Copia i file dalla cartella Dati alla cartella Backup
         for file_name in self.files_to_backup:
-            source_file = os.path.join(self.data_folder, file_name)
-            if os.path.exists(source_file):
-                dest_file = os.path.join(self.backup_folder, file_name)
-                shutil.copy(source_file, dest_file)
+            file_sorgente = os.path.join(self.cartella_dati, file_name)  #percorso originario
+            if os.path.exists(file_sorgente):
+                dest_file = os.path.join(self.cartella_backup, file_name) #percorso finale
+                shutil.copy(file_sorgente, dest_file)
                 self.msg_box.setText(f"Backup del file {file_name} completato.")
                 self.msg_box.setIcon(QMessageBox.Warning)
                 self.msg_box.exec_()
