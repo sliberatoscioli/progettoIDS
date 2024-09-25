defdb1594b98d35e33ca0d9a84a444a782d8142d
sliberatoscioli <sara.liberatoscioli2003@gmail.com>
2024-09-25 19:34:35 +0200

    commenti su controls, attivita e quasi tutte le viste

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # main.py
-
 import sys
 from PyQt5.QtWidgets import QApplication
 from Viste.vista_login import LoginForm
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    login_form = LoginForm()  # Crea un'istanza di LoginForm
-    login_form.showFullScreen()  # Mostra la finestra in modalità a schermo intero
-    sys.exit(app.exec_())  # Avvia il loop dell'applicazione
+    login_form = LoginForm()  # Creazione di un'istanza di LoginForm
+    login_form.showFullScreen()  # Finestra in modalità a schermo intero
+    sys.exit(app.exec_())  # Avvio del loop dell'applicazione
